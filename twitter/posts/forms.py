1e7996413a2fe563096d3eea62e18eb45cfc68be
savantakash <88370309+savantakash@users.noreply.github.com>
2021-08-26 13:28:37 +0530

    Delete twitter directory

--- twitter/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import fields
-from .models import Post
-
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
